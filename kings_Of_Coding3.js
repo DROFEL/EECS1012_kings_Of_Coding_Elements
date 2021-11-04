26e46bf75fe309cc29ecd658d6ae992ce3c19049
DROFEL <cradplay@gmail.com>
2021-11-04 14:15:27 +0300

    commit on main

--- kings_Of_Coding3.js
+++ kings_Of_Coding3.js
@@ -1,3 +1,5 @@
+var click = new Audio('../Source/Sounds/Click_1.mp3');
+
 function normal(){
 
 }
@@ -6,4 +8,17 @@ function playerVplayer(){
 }
 function impossible(){
   
+}
+
+function load(){
+    click.play();
+}
+
+function loadSounds(){
+    
+}
+
+function playSound(name){
+    
+    audio.play();
 }
